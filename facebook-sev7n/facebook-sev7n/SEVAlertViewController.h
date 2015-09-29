22ec91e41b65a9635225723a55cc3863f1f4a4e8
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-09-29 16:56:33 -0400

    Criando classe para implementar os alerts.

---
+++ facebook-sev7n/facebook-sev7n/SEVAlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  SEVAlertViewController.h
+//  facebook-sev7n
+//
+//  Created by Rodrigo Machado on 9/29/15.
+//  Copyright © 2015 sev7n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEVAlertViewController : UIViewController
+
+@end
---
+++ facebook-sev7n/facebook-sev7n/SEVAlertViewController.m
@@ -0,0 +1,37 @@
+//
+//  SEVAlertViewController.m
+//  facebook-sev7n
+//
+//  Created by Rodrigo Machado on 9/29/15.
+//  Copyright © 2015 sev7n. All rights reserved.
+//
+
+#import "SEVAlertViewController.h"
+
+@interface SEVAlertViewController ()
+
+@end
+
+@implementation SEVAlertViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
