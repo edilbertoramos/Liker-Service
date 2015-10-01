a94765603f145a4ca7c8b5c5c7a6cefb761e8a53
Rodrigo Machado <rodrigodangelo95@icloud.com>
2015-10-01 16:41:48 -0400

    Adicionando as classes para Meus Servicos

---
+++ facebook-sev7n/facebook-sev7n/SEVMeusServicosViewController.h
@@ -0,0 +1,13 @@
+//
+//  SEVMeusServicosViewController.h
+//  facebook-sev7n
+//
+//  Created by Rodrigo Machado on 10/1/15.
+//  Copyright © 2015 sev7n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEVMeusServicosViewController : UIViewController
+
+@end
---
+++ facebook-sev7n/facebook-sev7n/SEVMeusServicosViewController.m
@@ -0,0 +1,37 @@
+//
+//  SEVMeusServicosViewController.m
+//  facebook-sev7n
+//
+//  Created by Rodrigo Machado on 10/1/15.
+//  Copyright © 2015 sev7n. All rights reserved.
+//
+
+#import "SEVMeusServicosViewController.h"
+
+@interface SEVMeusServicosViewController ()
+
+@end
+
+@implementation SEVMeusServicosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
