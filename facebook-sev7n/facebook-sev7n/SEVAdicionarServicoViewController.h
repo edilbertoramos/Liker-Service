e54ae5a450f2d2fc6444be891f934ed7eeba0522
Rodrigo Machado <rodrigodangelo95@icloud.com>
2015-10-05 14:24:47 -0400

    Adicionando as classes da tela de adionar serviço

---
+++ facebook-sev7n/facebook-sev7n/SEVAdicionarServicoViewController.h
@@ -0,0 +1,13 @@
+//
+//  SEVAdicionarServicoViewController.h
+//  facebook-sev7n
+//
+//  Created by Rodrigo Machado on 10/5/15.
+//  Copyright © 2015 sev7n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEVAdicionarServicoViewController : UIViewController
+
+@end
